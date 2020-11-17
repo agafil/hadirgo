ac17bbf9fda2dbfb580c823326b9f8b43e673506
Charles C <71957934+Cattosan@users.noreply.github.com>
2020-11-17 19:33:57 +0700

    Update 1.1
    What's new?
    - Tampilan welcome screen
    - Database
    - Button

--- HADIRGo/target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.mycompany.hadirgo","l":"App"},{"p":"com.mycompany.hadirgo","l":"HadirGoDb"},{"p":"com.mycompany.hadirgo","l":"Home"},{"p":"com.mycompany.hadirgo","l":"MenuLogin"}]
