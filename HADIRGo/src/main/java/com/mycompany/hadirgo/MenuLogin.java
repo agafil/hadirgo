1c21de5a3c26e63fdb962a13342aa1a62f8e1c41
David-setiawan-W <david.setiawan@ti.ukdw.ac.id>
2020-11-21 23:18:10 +0700

    Revert "coba"
    This reverts commit 818eaec1e0619b039c4501d5dc1476a75a2997a8.

--- HADIRGo/src/main/java/com/mycompany/hadirgo/MenuLogin.java
+++ HADIRGo/src/main/java/com/mycompany/hadirgo/MenuLogin.java
@@ -11,7 +11,7 @@ public class MenuLogin {
     private static String usr;
     private String pwd;
     private boolean status = false;
-    private boolean iniAdminBukan = false;
+    
     @FXML
     private TextField username;
     @FXML
@@ -21,19 +21,15 @@ public class MenuLogin {
         usr = username.getText();
         pwd = password.getText();
         status = HadirGoDb.validate(usr, pwd);
-        iniAdminBukan = HadirGoDb.isAdmin(usr);
         
         if(status == true){
             App.setRoot("user");
         }
-//        else if(status == true){
-//            App.setRoot("dosen");
-//        }
         else{
             Alert alert = new Alert(AlertType.ERROR);
             alert.setTitle("Error");
             alert.setHeaderText(null);
-            alert.setContentText("User atau Password salah!");
+            alert.setContentText("User tidak ketemu!");
             alert.showAndWait();
         }
     }
