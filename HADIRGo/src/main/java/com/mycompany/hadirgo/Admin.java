818eaec1e0619b039c4501d5dc1476a75a2997a8
David-setiawan-W <david.setiawan@ti.ukdw.ac.id>
2020-11-21 23:18:02 +0700

    coba

---
+++ HADIRGo/src/main/java/com/mycompany/hadirgo/Admin.java
@@ -0,0 +1,23 @@
+package com.mycompany.hadirgo;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Admin {
+    @FXML
+    private Label labeluser;
+    @FXML
+    private void backToWelcome(ActionEvent event) throws IOException {
+        App.setRoot("Home");
+    }
+    @FXML
+    private void moveToDetailKelas() throws IOException{
+        App.setRoot("DetailKelas");
+    }
+    @FXML
+    public void initialize(){
+        labeluser.setText(MenuLogin.getUsername());
+    }    
+}
